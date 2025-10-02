0f9c0ba55e2a4d037dba52635a73f52affc91ca1
Guilherme Miranda <guilhermesilvamiranda227@gmail.com>
2025-10-02 15:35:47 -0300

    Update ConectionDB.py

--- ConectionDB.py
+++ ConectionDB.py
@@ -6,5 +6,32 @@ conexao = mysql.connector.connect(
     database="barbearia"     # nome do banco que você criou
 )
 cursor = conexao.cursor()
-cursor.execute
+cursor.execute("""
+
+CREATE DATABASE IF NOT EXISTS barbearia;
+
+USE barbearia;
+
+CREATE TABLE IF NOT EXISTS clientes (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    telefone VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS servicos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100),
+    preco DECIMAL(10,2)
+);
+
+CREATE TABLE IF NOT EXISTS agendamentos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    cliente_id INT,
+    servico_id INT,
+    data DATE,
+    FOREIGN KEY (cliente_id) REFERENCES clientes(id),
+    FOREIGN KEY (servico_id) REFERENCES servicos(id)
+);
+
 print("✅ Conectado e tabela criada (se não existia).")
+
